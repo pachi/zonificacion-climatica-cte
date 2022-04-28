77c4ab627ccd3ad22c4d5aacdb082cecd19e4ad7
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-04-28 17:56:14 +0200

    Script de selección de datos de entrada.
    Closes #11

--- src/select_input.py
+++ src/select_input.py
@@ -1,4 +1,6 @@
-"""SElección de datos de entrada de los municipios
+# encoding: utf-8
+
+"""Selección de datos de entrada de los municipios
 
 Selecciona los datos de entrada relevantes del archivo
 `data/ign/MUNICIPIOS.csv` del IGN y genera el archivo de
@@ -14,3 +16,39 @@ datos `data/output/Municipios.csv` que contendría:
 - ARCHIVO_TMY
 """
 
+import pandas as pd
+
+if __name__ == "__main__":
+    print("Cargando datos de municipios...")
+    df = pd.read_csv('../data/ign/MUNICIPIOS.csv',
+                     encoding='latin1',
+                     sep=';',
+                     decimal=',',
+                     dtype={'COD_INE': str,
+                            'ID_REL': str,
+                            'COD_GEO': str,
+                            'COD_PROV': str,
+                            'PROVINCIA': str,
+                            'NOMBRE_ACTUAL': str,
+                            'POBLACION_MUNI': int,
+                            'SUPERFICIE': float,
+                            'PERIMETRO': float,
+                            'COD_INE_CAPITAL': str,
+                            'CAPITAL': str,
+                            'POBLACION_CAPITAL': int,
+                            'HOJA_MTN25_ETRS89': str,
+                            'LONGITUD_ETRS89': float,
+                            'LATITUD_ETRS89': float,
+                            'ORIGENCOOR': str,
+                            'ALTITUD': float,
+                            'ORIGENALTITUD': str
+                            },
+                     usecols=[
+                         'COD_INE', 'COD_PROV', 'PROVINCIA', 'NOMBRE_ACTUAL',
+                         'LONGITUD_ETRS89', 'LATITUD_ETRS89', 'ALTITUD']
+                     )
+
+    df['ARCHIVO_TMY'] = df['COD_INE'].apply(lambda x: '{}.tmy'.format(x))
+
+    df.to_csv("../data/output/Municipios.csv", index=False)
+    print("Datos de {} municipios cargados.".format(len(df)))
