e0b00947c8efde54f9e972a0764907d497f5ec60
Rafael Villar Burke <pachi@ietcc.csic.es>
2022-05-04 12:34:09 +0200

    Cambia extensiones de gráficas a .png

--- src/plot_results.py
+++ src/plot_results.py
@@ -4,22 +4,24 @@ Obtiene diversas gráficas a partir de los datos en `data/output/Results.csv`:
 
 - mapas de zonas climáticas TMY:
 
-    - `data/output/graphics/zci-tmy.pdf`
-    - `data/output/graphics/zcv-tmy.pdf`
+    - `data/output/graphics/zci-tmy.png`
+    - `data/output/graphics/zcv-tmy.png`
 
 - mapas de zonas climáticas CTE:
 
-    - `data/output/graphics/zci-cte.pdf`
-    - `data/output/graphics/zcv-cte.pdf`
+    - `data/output/graphics/zci-cte.png`
+    - `data/output/graphics/zcv-cte.png`
 
 - mapas de diferencias en zonificación:
 
-    - `data/output/graphics/zci-diff.pdf`
-    - `data/output/graphics/zcv-diff.pdf`
+    - `data/output/graphics/zci-diff.png`
+    - `data/output/graphics/zcv-diff.png`
 
 - Histograma de magnitud de las diferencias entre zonificaciones:
 
-    - `data/output/graphics/zci-diff-hist.pdf`
-    - `data/output/graphics/zcv-diff-hist.pdf`
+    - `data/output/graphics/zci-diff-hist.png`
+    - `data/output/graphics/zcv-diff-hist.png`
 """
+
+
 
